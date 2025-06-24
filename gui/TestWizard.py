7745e0f8ce8bf8b24205d2c3c64cc87510e4833c
ratrobby <ratrobby@amazon.com>
2025-06-24 11:22:25 -0400

    Fix collapsible frame heights

--- gui/TestWizard.py
+++ gui/TestWizard.py
@@ -326,11 +326,10 @@ class TestWizard(tk.Tk):
         text_widget.insert("1.0", content)
 
         # Temporarily display to measure required height and then hide again
-        header.update_idletasks()
-        header_height = header.winfo_height()
+        header_height = header.winfo_reqheight()
         text_widget.pack(fill="x", padx=15, pady=2)
         text_widget.update_idletasks()
-        open_height = header_height + text_widget.winfo_height()
+        open_height = header_height + text_widget.winfo_reqheight()
         text_widget.pack_forget()
         container.configure(height=header_height)
         container.pack_propagate(False)  # maintain header height when collapsed
