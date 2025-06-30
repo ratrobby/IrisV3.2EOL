4d23463749d6d23fbfbdd166ac33abb2ed8fa90a
ratrobby <ratrobby@amazon.com>
2025-06-30 15:42:18 -0400

    Add Hold command for test scripts

---
+++ commands.py
@@ -0,0 +1,8 @@
+import time
+
+def hold(seconds):
+    """Pause execution for the specified number of seconds."""
+    time.sleep(float(seconds))
+
+# Alias with capital H for convenience
+Hold = hold
