75d7b6b6d8fa4ce4ad3120eaa2716523095b3f15
ratrobby <ratrobby@amazon.com>
2025-09-11 18:40:58 -0400

    Demonstrate calling Iris_EOL_Fixture functions

--- Sample_Test_Script.py
+++ Sample_Test_Script.py
@@ -1,6 +1,20 @@
 import Iris_EOL_Fixture
 
 if __name__ == "__main__":
-""" Read Loac Cell (Input Load Cell Number)"""
+    """Read Load Cell (input load cell number)"""
     Iris_EOL_Fixture.readLC(1)
 
+    """Read pressure from the PQ3834 sensor"""
+    Iris_EOL_Fixture.readPS()
+
+    """Read flow from the SD9500 sensor"""
+    Iris_EOL_Fixture.readVF()
+
+    """Read pressure from the SD9500 sensor"""
+    Iris_EOL_Fixture.readVP()
+
+    """Read flow from the SD6020 sensor"""
+    Iris_EOL_Fixture.readPF()
+
+    """Open monitoring window"""
+    Iris_EOL_Fixture.open_monitor()
