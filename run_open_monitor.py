95e7bff5a63412218e15b1c6bb0ed7862d93acb8
ratrobby <ratrobby@amazon.com>
2025-09-11 14:46:56 -0400

    Add open_monitor GUI with geometry and live sensor updates

--- run_open_monitor.py
+++ run_open_monitor.py
@@ -1,3 +1,7 @@
-import Iris_EOL_Sensor_Commands
+"""Utility script to launch the sensor monitoring window."""
+
+import Iris_EOL_Sensor_Commands as commands
+
+
 if __name__ == "__main__":
-    readPS()
+    commands.open_monitor()
